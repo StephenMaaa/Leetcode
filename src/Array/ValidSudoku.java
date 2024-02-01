aed8f26498d5e1f481d118e0754962700128b819
Jiachen Ma <jma@ucsd.edu>
2024-01-31 23:39:38 -0800

    Completed LeetCode 36 and 417 - Jan 31, 2024

--- src/Array/ValidSudoku.java
+++ src/Array/ValidSudoku.java
@@ -1,6 +1,8 @@
 package Array;
 
 /*
+LeetCode 36
+
 Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
         Each row must contain the digits 1-9 without repetition.
@@ -19,7 +21,7 @@ import java.util.Set;
 
 public class ValidSudoku {
     // approach 1 - Block Traversal + Map TC: O(9 * 9) SC: O(9 * 9 * 3)
-    public boolean isValidSudoku(char[][] board) {
+    public boolean isValidSudoku2(char[][] board) {
         Map<Integer, Set<Integer>> rowMap = new HashMap<>();
         Map<Integer, Set<Integer>> colMap = new HashMap<>();
 
@@ -69,35 +71,71 @@ public class ValidSudoku {
         return true;
     }
 
-    // approach 2 - Block Traversal + int array TC: O(9 * 9) SC: O(9 * 9 * 3)
-    public boolean isValidSudoku2(char[][] board) {
-        int[][] rowDup = new int[9][9];
-        int[][] colDup = new int[9][9];
-        int[][][] blockDup = new int[3][3][9];
+//    // approach 2 - Block Traversal + int array TC: O(9 * 9) SC: O(9 * 9 * 3)
+//    public boolean isValidSudoku2(char[][] board) {
+//        int[][] rowDup = new int[9][9];
+//        int[][] colDup = new int[9][9];
+//        int[][][] blockDup = new int[3][3][9];
+//
+//        // traverse blocks
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                char digit = board[i][j];
+//                if (digit == '.') {
+//                    continue;
+//                }
+//                int index = digit - '0' - 1;
+//                rowDup[i][index]++;
+//                colDup[j][index]++;
+//                blockDup[i / 3][j / 3][index]++;
+//
+//                // check dup
+//                if (rowDup[i][index] > 1 || colDup[j][index] > 1 || blockDup[i / 3][j / 3][index] > 1) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
 
-        // traverse blocks
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char digit = board[i][j];
-                if (digit == '.') {
-                    continue;
-                }
-                int index = digit - '0' - 1;
-                rowDup[i][index]++;
-                colDup[j][index]++;
-                blockDup[i / 3][j / 3][index]++;
+    // approach 1: Map + Set TC: O(n^2) SC: O(n^2)
+    public boolean isValidSudoku(char[][] board) {
+        // initialization
+        Map<Integer, Set<Character>> rowMap = new HashMap<>();
+        Map<Integer, Set<Character>> colMap = new HashMap<>();
+        Map<Integer, Set<Character>> blockMap = new HashMap<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                rowMap.put(i * 3 + j, new HashSet<>());
+                colMap.put(i * 3 + j, new HashSet<>());
+                blockMap.put(i * 10 + j, new HashSet<>());
+            }
+        }
 
-                // check dup
-                if (rowDup[i][index] > 1 || colDup[j][index] > 1 || blockDup[i / 3][j / 3][index] > 1) {
-                    return false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                // check
+                if (board[i][j] != '.') {
+                    // check dup
+                    if (rowMap.get(i).contains(board[i][j]) || colMap.get(j).contains(board[i][j]) || blockMap.get((i / 3) * 10 + (j / 3)).contains(board[i][j])) {
+                        return false;
+                    }
+
+                    // update
+                    rowMap.get(i).add(board[i][j]);
+                    colMap.get(j).add(board[i][j]);
+                    blockMap.get((i / 3) * 10 + (j / 3)).add(board[i][j]);
                 }
             }
         }
         return true;
     }
 
-//    public static void main(String[] args) {
-//        ValidSudoku test = new ValidSudoku();
-//        char[][] board = new char[][]{}
-//    }
+    public static void main(String[] args) {
+        ValidSudoku test = new ValidSudoku();
+        char[][] board = new char[][]{{'5','3','.','.','7','.','.','.','.'}, {'6','.','.','1','9','5','.','.','.'}, {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'}, {'4','.','.','8','.','3','.','.','1'}, {'7','.','.','.','2','.','.','.','6'}, {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'}, {'.','.','.','.','8','.','.','7','9'}};
+        System.out.println(test.isValidSudoku(board));
+    }
 }
--- src/BFS/PacificAtlanticFlow.java
+++ src/BFS/PacificAtlanticFlow.java
@@ -1,6 +1,8 @@
 package BFS;
 
 /*
+LeetCode 417
+
 Given an m * n matrix of non-negative integers representing the height of each unit cell in a continent, the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" touches the right and bottom edges.
 
         Water can only flow in four directions (up, down, left, or right) from a cell to another one with height equal or lower.
@@ -30,119 +32,204 @@ Given an m * n matrix of non-negative integers representing the height of each u
 import java.util.*;
 
 public class PacificAtlanticFlow {
-    // time complexity: O(mn)
-    // space complexity: O(mn)
-    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
-        List<List<Integer>> res = new ArrayList<>();
-        // edge case
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return res;
+//    // time complexity: O(mn)
+//    // space complexity: O(mn)
+//    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+//        List<List<Integer>> res = new ArrayList<>();
+//        // edge case
+//        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+//            return res;
+//        }
+//
+//        int rows = matrix.length;
+//        int cols = matrix[0].length;
+//        int[][] pacific = generatePacific(matrix);
+//        int[][] atlantic = generateAtlantic(matrix);
+//
+//        // check
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < cols; j++) {
+//                if (pacific[i][j] == 1 && atlantic[i][j] == 1) {
+//                    res.add(Arrays.asList(i, j));
+//                }
+//            }
+//        }
+//        return res;
+//    }
+//
+//    private int[][] generatePacific(int[][] matrix) {
+//        // BFS
+//        int rows = matrix.length;
+//        int cols = matrix[0].length;
+//        int[][] pacific = new int[rows][cols];
+//        Deque<int[]> queue = new ArrayDeque<>();
+//        boolean[][] visited = new boolean[rows][cols];
+//        int[][] directions = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+//
+//        // initialization
+//        for (int i = 0; i < cols; i++) {
+//            if (!visited[0][i]) {
+//                pacific[0][i] = 1;
+//                queue.add(new int[]{0, i});
+//                visited[0][i] = true;
+//            }
+//        }
+//
+//        for (int i = 0; i < rows; i++) {
+//            if (!visited[i][0]) {
+//                pacific[i][0] = 1;
+//                queue.offer(new int[]{i, 0});
+//                visited[i][0] = true;
+//            }
+//        }
+//
+//        while (!queue.isEmpty()) {
+//            int[] entry = queue.poll();
+//            int i = entry[0];
+//            int j = entry[1];
+//
+//            // expand
+//            for (int[] dir : directions) {
+//                int row = i + dir[0];
+//                int col = j + dir[1];
+//                if (row >= 0 && row < rows && col >= 0 && col < cols && matrix[i][j] <= matrix[row][col] && !visited[row][col]) {
+//                    pacific[row][col] = 1;
+//                    queue.offer(new int[]{row, col});
+//                    visited[row][col] = true;
+//                }
+//            }
+//        }
+//        return pacific;
+//    }
+//
+//    private int[][] generateAtlantic(int[][] matrix) {
+//        // BFS
+//        int rows = matrix.length;
+//        int cols = matrix[0].length;
+//        int[][] atlantic = new int[rows][cols];
+//        Deque<int[]> queue = new ArrayDeque<>();
+//        boolean[][] visited = new boolean[rows][cols];
+//        int[][] directions = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+//
+//        // initialization
+//        for (int i = cols - 1; i >= 0; i--) {
+//            if (!visited[rows - 1][i]) {
+//                atlantic[rows - 1][i] = 1;
+//                queue.add(new int[]{rows - 1, i});
+//                visited[rows - 1][i] = true;
+//            }
+//        }
+//
+//        for (int i = rows - 1; i >= 0; i--) {
+//            if (!visited[i][cols - 1]) {
+//                atlantic[i][cols - 1] = 1;
+//                queue.offer(new int[]{i, cols - 1});
+//                visited[i][cols - 1] = true;
+//            }
+//        }
+//
+//        while (!queue.isEmpty()) {
+//            int[] entry = queue.poll();
+//            int i = entry[0];
+//            int j = entry[1];
+//
+//            // expand
+//            for (int[] dir : directions) {
+//                int row = i + dir[0];
+//                int col = j + dir[1];
+//                if (row >= 0 && row < rows && col >= 0 && col < cols && matrix[i][j] <= matrix[row][col] && !visited[row][col]) {
+//                    atlantic[row][col] = 1;
+//                    queue.offer(new int[]{row, col});
+//                    visited[row][col] = true;
+//                }
+//            }
+//        }
+//        return atlantic;
+//    }
+
+    // approach 1: BFS TC: O(mn) SC: O(mn)
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        // initialization for pacific
+        int rows = heights.length;
+        int cols = heights[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[rows][cols];
+        int[][] directions = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+        // top left to bottom right
+        for (int i = 0; i < rows; i++) {
+            queue.offer(new int[]{i, 0});
+            visited[i][0] = true;
         }
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] pacific = generatePacific(matrix);
-        int[][] atlantic = generateAtlantic(matrix);
+        for (int j = 0; j < cols; j++) {
+            queue.offer(new int[]{0, j});
+            visited[0][j] = true;
+        }
 
-        // check
+        // BFS
+        while (!queue.isEmpty()) {
+            int[] entry = queue.poll();
+            int i = entry[0];
+            int j = entry[1];
+
+            // check neighbors
+            for (int[] dir : directions) {
+                int row = i + dir[0];
+                int col = j + dir[1];
+
+                // check
+                if (row >= 0 && row < rows && col >= 0 && col < cols && heights[i][j] <= heights[row][col] && !visited[row][col]) {
+                    queue.offer(new int[]{row, col});
+                    visited[row][col] = true;
+                }
+            }
+        }
+
+        // initialization for atlantic
+        boolean[][] visited2 = new boolean[rows][cols];
+
+        // bottom right to top left
+        for (int i = 0; i < rows; i++) {
+            queue.offer(new int[]{i, cols - 1});
+            visited2[i][cols - 1] = true;
+        }
+
+        for (int j = 0; j < cols; j++) {
+            queue.offer(new int[]{rows - 1, j});
+            visited2[rows - 1][j] = true;
+        }
+
+        // BFS
+        while (!queue.isEmpty()) {
+            int[] entry = queue.poll();
+            int i = entry[0];
+            int j = entry[1];
+
+            // check neighbors
+            for (int[] dir : directions) {
+                int row = i + dir[0];
+                int col = j + dir[1];
+
+                // check
+                if (row >= 0 && row < rows && col >= 0 && col < cols && heights[i][j] <= heights[row][col] && !visited2[row][col]) {
+                    queue.offer(new int[]{row, col});
+                    visited2[row][col] = true;
+                }
+            }
+        }
+
+        // process
+        List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (pacific[i][j] == 1 && atlantic[i][j] == 1) {
+                // check
+                if (visited[i][j] && visited2[i][j]) {
                     res.add(Arrays.asList(i, j));
                 }
             }
         }
-        return res;
-    }
-
-    private int[][] generatePacific(int[][] matrix) {
-        // BFS
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] pacific = new int[rows][cols];
-        Deque<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[rows][cols];
-        int[][] directions = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-
-        // initialization
-        for (int i = 0; i < cols; i++) {
-            if (!visited[0][i]) {
-                pacific[0][i] = 1;
-                queue.add(new int[]{0, i});
-                visited[0][i] = true;
-            }
-        }
-
-        for (int i = 0; i < rows; i++) {
-            if (!visited[i][0]) {
-                pacific[i][0] = 1;
-                queue.offer(new int[]{i, 0});
-                visited[i][0] = true;
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int[] entry = queue.poll();
-            int i = entry[0];
-            int j = entry[1];
-
-            // expand
-            for (int[] dir : directions) {
-                int row = i + dir[0];
-                int col = j + dir[1];
-                if (row >= 0 && row < rows && col >= 0 && col < cols && matrix[i][j] <= matrix[row][col] && !visited[row][col]) {
-                    pacific[row][col] = 1;
-                    queue.offer(new int[]{row, col});
-                    visited[row][col] = true;
-                }
-            }
-        }
-        return pacific;
-    }
-
-    private int[][] generateAtlantic(int[][] matrix) {
-        // BFS
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] atlantic = new int[rows][cols];
-        Deque<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[rows][cols];
-        int[][] directions = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-
-        // initialization
-        for (int i = cols - 1; i >= 0; i--) {
-            if (!visited[rows - 1][i]) {
-                atlantic[rows - 1][i] = 1;
-                queue.add(new int[]{rows - 1, i});
-                visited[rows - 1][i] = true;
-            }
-        }
-
-        for (int i = rows - 1; i >= 0; i--) {
-            if (!visited[i][cols - 1]) {
-                atlantic[i][cols - 1] = 1;
-                queue.offer(new int[]{i, cols - 1});
-                visited[i][cols - 1] = true;
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int[] entry = queue.poll();
-            int i = entry[0];
-            int j = entry[1];
-
-            // expand
-            for (int[] dir : directions) {
-                int row = i + dir[0];
-                int col = j + dir[1];
-                if (row >= 0 && row < rows && col >= 0 && col < cols && matrix[i][j] <= matrix[row][col] && !visited[row][col]) {
-                    atlantic[row][col] = 1;
-                    queue.offer(new int[]{row, col});
-                    visited[row][col] = true;
-                }
-            }
-        }
-        return atlantic;
+        return res; 
     }
 
     public static void main(String[] args) {
