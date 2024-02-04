79259586201bad54d02bfff53f708218029b16fe
Jiachen Ma <jma@ucsd.edu>
2024-02-04 00:42:28 -0800

    Completed LeetCode 128 and 210 - Feb 3, 2024

--- src/Array/LongestConsecutiveSequence.java
+++ src/Array/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package Array;
 
 /*
+LeetCode 128
+
 Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 
         For example,
@@ -11,107 +13,134 @@ Given an unsorted array of integers, find the length of the longest consecutive 
 import java.util.*;
 
 public class LongestConsecutiveSequence {
-    // approach 1 - Sort + Linear Scan TC: O(nlogn) SC: O(logn)
-    public int longestConsecutive(int[] array) {
-        // sort
-        Arrays.sort(array);
+//    // approach 1 - Sort + Linear Scan TC: O(nlogn) SC: O(logn)
+//    public int longestConsecutive(int[] array) {
+//        // sort
+//        Arrays.sort(array);
+//
+//        int max = 0;
+//        int count = 0;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            // case 1: consecutive
+//            // case 2: otherwise
+//            if (i == 0 || array[i] == array[i - 1] + 1) {
+//                count++;
+//            } else {
+//                count = 1;
+//            }
+//
+//            max = Math.max(max, count);
+//        }
+//        return max;
+//    }
+//
+//    // approach 2 - Union Find TC: O(nlogn) SC: O(n)
+//    public int longestConsecutive2(int[] nums) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        int[] parent = new int[nums.length];
+//        int[] size = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            parent[i] = i;
+//            size[i] = 1;
+//        }
+//
+//        // union find
+//        for (int i = 0; i < nums.length; i++) {
+//            if (!map.containsKey(nums[i])) {
+//                // check left and right
+//                if (map.containsKey(nums[i] - 1)) {
+//                    union(i, map.get(nums[i] - 1), parent, size);
+//                }
+//
+//                if (map.containsKey(nums[i] + 1)) {
+//                    union(i, map.get(nums[i] + 1), parent, size);
+//                }
+//
+//                map.put(nums[i], i);
+//            }
+//        }
+//
+//        // update max
+//        int max = 0;
+//        for (int i = 0; i < parent.length; i++) {
+//            if (i == parent[i]) {
+//                max = Math.max(max, size[i]);
+//            }
+//        }
+//        return max;
+//    }
+//
+//    private void union(int u, int v, int[] parent, int[] size) {
+//        u = find(u, parent);
+//        v = find(v, parent);
+//
+//        if (u == v) {
+//            return;
+//        }
+//
+//        if (size[u] < size[v]) {
+//            parent[u] = v;
+//            size[v] += size[u];
+//        } else {
+//            parent[v] = u;
+//            size[u] += size[v];
+//        }
+//    }
+//
+//    private int find(int i, int[] parent) {
+//        // base case
+//        if (i != parent[i]) {
+//            parent[i] = find(parent[i], parent);
+//        }
+//        return parent[i];
+//    }
+//
+//    // approach 3 - Linear Scan TC: O(n) SC: O(n)
+//    public int longestConsecutive3(int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            set.add(nums[i]);
+//        }
+//        int max = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            // find the start of a sequence
+//            if (!set.contains(nums[i] - 1)) {
+//                int curr = nums[i];
+//                int count = 1;
+//                while (set.contains(curr + 1)) {
+//                    curr++;
+//                    count++;
+//                }
+//
+//                max = Math.max(max, count);
+//            }
+//        }
+//        return max;
+//    }
 
-        int max = 0;
-        int count = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            // case 1: consecutive
-            // case 2: otherwise
-            if (i == 0 || array[i] == array[i - 1] + 1) {
-                count++;
-            } else {
-                count = 1;
-            }
-
-            max = Math.max(max, count);
-        }
-        return max;
-    }
-
-    // approach 2 - Union Find TC: O(nlogn) SC: O(n)
-    public int longestConsecutive2(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] parent = new int[nums.length];
-        int[] size = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-
-        // union find
-        for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                // check left and right
-                if (map.containsKey(nums[i] - 1)) {
-                    union(i, map.get(nums[i] - 1), parent, size);
-                }
-
-                if (map.containsKey(nums[i] + 1)) {
-                    union(i, map.get(nums[i] + 1), parent, size);
-                }
-
-                map.put(nums[i], i);
-            }
-        }
-
-        // update max
-        int max = 0;
-        for (int i = 0; i < parent.length; i++) {
-            if (i == parent[i]) {
-                max = Math.max(max, size[i]);
-            }
-        }
-        return max;
-    }
-
-    private void union(int u, int v, int[] parent, int[] size) {
-        u = find(u, parent);
-        v = find(v, parent);
-
-        if (u == v) {
-            return;
-        }
-
-        if (size[u] < size[v]) {
-            parent[u] = v;
-            size[v] += size[u];
-        } else {
-            parent[v] = u;
-            size[u] += size[v];
-        }
-    }
-
-    private int find(int i, int[] parent) {
-        // base case
-        if (i != parent[i]) {
-            parent[i] = find(parent[i], parent);
-        }
-        return parent[i];
-    }
-
-    // approach 3 - Linear Scan TC: O(n) SC: O(n)
-    public int longestConsecutive3(int[] nums) {
+    // approach 1: Set + Linear Scan TC: O(n) SC: O(n)
+    public int longestConsecutive(int[] nums) {
+        // initialization
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+        for (int i : nums) {
+            set.add(i);
         }
+
         int max = 0;
+        // scan
         for (int i = 0; i < nums.length; i++) {
-            // find the start of a sequence
+            // check
             if (!set.contains(nums[i] - 1)) {
                 int curr = nums[i];
                 int count = 1;
+
+                // update
                 while (set.contains(curr + 1)) {
                     curr++;
                     count++;
                 }
-
-                max = Math.max(max, count);
+                max = Math.max(count, max);
             }
         }
         return max;
@@ -120,6 +149,6 @@ public class LongestConsecutiveSequence {
     public static void main(String[] args) {
         LongestConsecutiveSequence test = new LongestConsecutiveSequence();
         int[] arr = new int[]{100, 4, 4, 200, 1, 3, 2};
-        System.out.println(test.longestConsecutive3(arr));
+        System.out.println(test.longestConsecutive(arr));
     }
 }
--- src/Graph/CourseScheduleII.java
+++ src/Graph/CourseScheduleII.java
@@ -1,6 +1,8 @@
 package Graph;
 
 /*
+LeetCode 210
+
 There are a total of n courses you have to take, labeled from 0 to n - 1.
 
         Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
@@ -21,45 +23,89 @@ There are a total of n courses you have to take, labeled from 0 to n - 1.
 import java.util.*;
 
 public class CourseScheduleII {
-    // time complexity: O(n)
-    // space complexity: O(n)
+//    // time complexity: O(n)
+//    // space complexity: O(n)
+//    public int[] findOrder(int numCourses, int[][] prerequisites) {
+//        // initialize the graph
+//        Map<Integer, List<Integer>> indegreeMap = new HashMap<>();
+//        for (int[] preq : prerequisites) {
+//            if (!indegreeMap.containsKey(preq[1])) {
+//                indegreeMap.put(preq[1], new ArrayList<>());
+//            }
+//            indegreeMap.get(preq[1]).add(preq[0]);
+//        }
+//
+//        // initialize indegrees of each vertex
+//        int[] res = new int[numCourses];
+//        int[] indegreeArr = new int[numCourses];
+//        for (Integer key : indegreeMap.keySet()) {
+//            for (Integer val : indegreeMap.get(key)) {
+//                indegreeArr[val]++;
+//            }
+//        }
+//
+//        // initialize queue with 0-indegree vertices
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        for (int i = 0; i < numCourses; i++) {
+//            if (indegreeArr[i] == 0) {
+//                queue.add(i);
+//            }
+//        }
+//
+//        // topological sort
+//        int count = 0;
+//        while (!queue.isEmpty()) {
+//            int course = queue.poll();
+//            res[count++] = course;
+//            if (indegreeMap.containsKey(course)) {
+//                for (Integer n : indegreeMap.get(course)) {
+//                    indegreeArr[n]--;
+//                    if (indegreeArr[n] == 0) {
+//                        queue.add(n);
+//                    }
+//                }
+//            }
+//        }
+//        return count == numCourses ? res : new int[0];
+//    }
+
+    // approach 1: Graph + BFS TC: O(V + E) SC: O(V + E)
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // initialize the graph
-        Map<Integer, List<Integer>> indegreeMap = new HashMap<>();
+        // initialization
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] indegrees = new int[numCourses];
         for (int[] preq : prerequisites) {
-            if (!indegreeMap.containsKey(preq[1])) {
-                indegreeMap.put(preq[1], new ArrayList<>());
+            // init
+            if (!map.containsKey(preq[1])) {
+                map.put(preq[1], new ArrayList<>());
             }
-            indegreeMap.get(preq[1]).add(preq[0]);
+            map.get(preq[1]).add(preq[0]);
+            indegrees[preq[0]]++;
         }
 
-        // initialize indegrees of each vertex
-        int[] res = new int[numCourses];
-        int[] indegreeArr = new int[numCourses];
-        for (Integer key : indegreeMap.keySet()) {
-            for (Integer val : indegreeMap.get(key)) {
-                indegreeArr[val]++;
-            }
-        }
-
-        // initialize queue with 0-indegree vertices
         Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < numCourses; i++) {
-            if (indegreeArr[i] == 0) {
-                queue.add(i);
+            // check origin/start
+            if (indegrees[i] == 0) {
+                queue.offer(i);
             }
         }
 
-        // topological sort
         int count = 0;
+        int[] res = new int[numCourses];
         while (!queue.isEmpty()) {
             int course = queue.poll();
             res[count++] = course;
-            if (indegreeMap.containsKey(course)) {
-                for (Integer n : indegreeMap.get(course)) {
-                    indegreeArr[n]--;
-                    if (indegreeArr[n] == 0) {
-                        queue.add(n);
+
+            // expand
+            if (map.containsKey(course)) {
+                for (int nextCourse : map.get(course)) {
+                    // update
+                    indegrees[nextCourse]--;
+
+                    // check
+                    if (indegrees[nextCourse] == 0) {
+                        queue.offer(nextCourse);
                     }
                 }
             }
